3e2660f9129b5268a258518d4e51ecb710751497
Zebfred <zebfred22@gmail.com>
2019-12-06 13:49:11 -0800

    an update

--- sprint-challenge/part5_test.py
+++ sprint-challenge/part5_test.py
@@ -1,5 +1,5 @@
 import unittest 
-from part1_acme import Product
+from part1_acme import Product, BoxingGlove
 from part4_report import generate_products, ADJECTIVES, NOUNS
 
 class AcmeProductTests(unittest.TestCase):
@@ -8,6 +8,33 @@ class AcmeProductTests(unittest.TestCase):
         """Test default product price being 10."""
         prod = Product('Test Product')
         self.assertEqual(prod.price, 10)
+        
+    def test_default_product_weight(self):
+        """Test default product weight being 20."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.weight, 20)
+        
+    def test_default_product_flannability(self):
+        """Test default product price being .05."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.flannability, 0.5)    
+    def test_the_gloves(self):
+        """Test glove price"""
+        glove = BoxingGlove('A good puncher')
+        self.assertEqual(glove.price,100 )
+        
+    def test_stealability(self):
+        """stealable? """
+        Anvil = Product('Acme Anvil', price= 40, weight = 100, flannibility= .1)
+        self.assertEqual(Anvil.stealability(),"Not so stealable...")
+    def test_stealability(self):
+        """flammable? """
+class AcmeReportTests(unittest.TestCase):
+    """Making sure we make Acme products right."""
+    def test_default_num_products(self):
+        "30 products eh?"
+        self.assertEqual(len(generate_products()), 30)
+        
 
 if __name__ == '__main__':
     unittest.main()
