441108fc1d0250e8dcb26fd5e76b16a54d91edc2
Zebfred <zebfred22@gmail.com>
2019-12-06 14:06:21 -0800

    part 6

--- sprint-challenge/part4_report.py
+++ sprint-challenge/part4_report.py
@@ -18,36 +18,36 @@ def generate_products(num_products=30):
     products = []
     """Adding random products for part4"""
     for a_thing in range(num_products):
-        The_product = sample(ADJECTIVES,1)[0] + '' + sample(NOUNS,1)[0]
+        The_product = sample(ADJECTIVES, 1)[0] + '' + sample(NOUNS, 1)[0]
         price = randint(5, 100)
         weight = randint(5, 100)
         flannability = uniform(0.0, 2.5)
-        products.append(Product(The_product, price=price, weight=weight, flannability = flannability))
-        
-    return products
+        products.append(Product(The_product, price=price,
+                                weight=weight, flannability=flannability))
 
+    return products
 
 
 def inventory_report(products):
     """Yep this is a doc string """
-    unique_names =  set()
+    unique_names = set()
     a_price = 0
-    a_weight  = 0
+    a_weight = 0
     a_flannability = 0.0
     for another_thing in products:
         unique_names.add(another_thing.name)
         a_price += another_thing.price
         a_weight += another_thing.weight
         a_flannability += another_thing.flannability
-    
+
     print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
     print('Unique product names:')
     print(len(unique_names))
     print('Average price is:')
     print(a_price/len(products))
-    print('Average weight:')  
+    print('Average weight:')
     print(a_weight/len(products))
-    print('Average flammability:')     
+    print('Average flammability:')
     print(a_flannability/len(products))
 
 
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     inventory_report(generate_products())
 
 
-#The last lines let you test by running `python acme_report.py`
+# The last lines let you test by running `python acme_report.py`
 '''
 $ python acme_report.py 
 ACME CORPORATION OFFICIAL INVENTORY REPORT
--- sprint-challenge/part6onword.md
+++ sprint-challenge/part6onword.md
@@ -3,7 +3,7 @@ part6 dont have time for it
 part7 
 
 - What, in your opinion, is an important part of code reviews?
-To learn to read others code, help each other improve their code, and think about an approach different from your own. Why do I need 5 sentences to explain that?
+code reviews are important because we can learn to read others code. we can help each other improve our code. we can have some one verify that we are pep8 compliant. gives the oppitunity to think about an approach different from your own. code reviews are important. 
   
 - How could containers help us
   improve this situation?
